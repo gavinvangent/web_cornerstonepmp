7b0bc7fd7168af58429e2cace2e36055835f40f3
Gavin van Gent <gavinvangent@hotmail.com>
2017-03-06 02:04:43 +0200

    Atted title to tab header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,16 +18,17 @@ window.cpmpApp = angular.module('cpmpApp', [
   ]);
 
 window.cpmpApp.config(['$httpProvider', '$resourceProvider', '$routeProvider', function($httpProvider, $resourceProvider, $routeProvider){
-	$routeProvider.when('/', { templateUrl: 'views/home.html', controller: 'HomeCtrl', menu: 'home', footer: 'default' });
-    $routeProvider.when('/about', { templateUrl: 'views/about.html', controller: 'AboutCtrl', menu: 'about', footer: 'default' });
-    $routeProvider.when('/contact', { templateUrl: 'views/contact.html', controller: 'ContactCtrl', menu: 'contact' });
-    $routeProvider.when('/portfolio', { templateUrl: 'views/portfolio.html', controller: 'PortfolioCtrl', menu: 'portfolio', footer: 'default' });
-    $routeProvider.when('/services', { templateUrl: 'views/services.html', controller: 'ServicesCtrl', menu: 'services', footer: 'default' });
+	$routeProvider.when('/', { templateUrl: 'views/home.html', controller: 'HomeCtrl', menu: 'home', footer: 'default', title: 'Home' });
+    $routeProvider.when('/about', { templateUrl: 'views/about.html', controller: 'AboutCtrl', menu: 'about', footer: 'default', title: 'About Us' });
+    $routeProvider.when('/contact', { templateUrl: 'views/contact.html', controller: 'ContactCtrl', menu: 'contact', title: 'Contact Us' });
+    $routeProvider.when('/portfolio', { templateUrl: 'views/portfolio.html', controller: 'PortfolioCtrl', menu: 'portfolio', footer: 'default', title: 'Portfolio' });
+    $routeProvider.when('/services', { templateUrl: 'views/services.html', controller: 'ServicesCtrl', menu: 'services', footer: 'default', title: 'Services' });
     $routeProvider.otherwise({ redirectTo: '/' });
 }]);
 
 window.cpmpApp.run(['$rootScope', function($rootScope) {
 	$rootScope.$on('$routeChangeStart', function(event, next){
+        $rootScope.title = next.$$route.title;
 		$rootScope.menu = next.$$route.menu;
         $rootScope.footer = next.$$route.footer;
 	});
